582b4df02d47d67f807e11500ffa5610289a69c2
The-Real-Martaz <abel.evans1226@gmail.com>
2025-05-09 12:07:12 -0400

    Add files via upload

---
+++ style.cs
@@ -0,0 +1,20 @@
+/* Make the whole page a flex container */
+body, html {
+  height: 100%;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: Arial, sans-serif;
+}
+
+.container {
+  margin: auto
+  text-align: center;
+  max-width: 900px; /* You can adjust this based on your design */
+  width: 100%;
+}
+
+header, main, footer {
+  margin: 20px;
+}
